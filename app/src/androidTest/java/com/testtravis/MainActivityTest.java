1613dfb7d142576bfe96c723075238b8f76c20e9
azhan <m.azhanmizani21@gmail.com>
2018-12-18 09:44:16 +0700

    Travis Test 23

---
+++ app/src/androidTest/java/com/testtravis/MainActivityTest.java
@@ -0,0 +1,44 @@
+package com.testtravis;
+
+import android.support.test.rule.ActivityTestRule;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.junit.Assert.*;
+
+public class MainActivityTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> activityActivityTestRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void testButton() {
+        onView(withId(R.id.inputET))
+                .perform(typeText("Test"))
+                .perform(closeSoftKeyboard());
+
+        onView(withId(R.id.clickButton))
+                .perform(click());
+
+        onView(withId(R.id.labelTV))
+                .check(matches(withText("Test")));
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+}
--- app/src/main/java/com/testtravis/MainActivity.java
+++ app/src/main/java/com/testtravis/MainActivity.java
@@ -2,12 +2,37 @@ package com.testtravis;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView labelTV;
+    private Button clickButton;
+    private EditText inputET;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bindingView();
+        clickButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!inputET.getText().toString().trim().isEmpty()) {
+                    labelTV.setText(inputET.getText().toString());
+                } else {
+                    labelTV.setText("Input is empty");
+                }
+            }
+        });
+    }
+
+    private void bindingView() {
+        labelTV = findViewById(R.id.labelTV);
+        clickButton = findViewById(R.id.clickButton);
+        inputET = findViewById(R.id.inputET);
     }
 }
